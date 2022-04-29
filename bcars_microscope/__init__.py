7d4e43157d3fe2a1c67f38f43c41f6a7f3cc7591
Charles H. Camp Jr <charles.camp@nist.gov>
2022-04-29 16:32:22 -0400

    Adjust style sheey

--- bcars_microscope/__init__.py
+++ bcars_microscope/__init__.py
@@ -5,6 +5,7 @@ from ._version import __version__
 dark_style_sheet = ('QWidget {background-color: rgb(53,53,53); font: 11pt "Arial";}\n'
                     'QLabel {color: white}\n'
                     'QPushButton {background-color: rgb(53,53,53); color: rgb(255,255,255)}\n'
+                    'QPushButton:disabled {color: rgb(100,100,100)}\n'
                     'QAbstractSpinBox {background-color: rgb(255,255,255)}\n'
                     'QCheckBox {color: red; background-color: white; width: 0; height: 0; spacing: 0px}\n'
                     'QCheckBox::indicator {color: red; width: 25; height: 25}\n'
@@ -12,7 +13,9 @@ dark_style_sheet = ('QWidget {background-color: rgb(53,53,53); font: 11pt "Arial
                     'QRadioButton::indicator {width:20; height: 20; color: black; border-radius: 10px}\n'
                     'QRadioButton::indicator:checked {background-color: rgb(85, 255, 0); border: 2px solid white}\n'
                     'QRadioButton::indicator:unchecked {background-color: rgb(100,100,100); border: 2px solid white}\n'
-                    'QDoubleSpinBox {font: 11pt "Arial";}\n')
+                    'QDoubleSpinBox {font: 11pt "Arial";}\n'
+                    'QGroupBox {color: white}\n'
+                    'QFrame {color: white}\n')
 
 # QRadioButton { color: white}\nQRadioButton::indicator { width: 50; height: 50; border-radius: 20px}\nQRadioButton::indicator:checked {background-color: rgb(85, 255, 0); border: 2px solid white}\nQRadioButton::indicator:unchecked {background-color: rgb(100,100,100); border: 2px solid white}
 
