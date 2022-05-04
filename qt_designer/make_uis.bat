f493aa72afd63b4b1ca48851e38ed7fa25ee9031
Charles H. Camp Jr <charles.camp@nist.gov>
2022-05-04 17:13:24 -0400

    Added learn_multithread stuff

--- qt_designer/make_uis.bat
+++ qt_designer/make_uis.bat
@@ -2,3 +2,4 @@ pyside2-uic.exe .\qt_bcars2_raster.ui -o ..\bcars_microscope\ui\ui_bcars2_raster
 pyside2-uic.exe .\qt_bcars2_spectroscopy.ui -o ..\bcars_microscope\ui\ui_bcars2_spectroscopy.py
 pyside2-uic.exe .\qt_demo_2_threads.ui -o ..\bcars_microscope\ui\ui_demo_2_threads.py
 pyside2-uic.exe .\qt_bcars2_main.ui -o ..\bcars_microscope\ui\ui_bcars2_main.py
+pyside2-uic.exe .\qt_learn_multithread.ui -o ..\bcars_microscope\dev_edu\ui_learn_multithread.py
