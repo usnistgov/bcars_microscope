797b964b8a5523a957c49da8cef2436f288f4f36
Charles H. Camp Jr <charles.camp@nist.gov>
2022-05-11 13:00:46 -0400

    Working on CCD config

--- qt_designer/make_uis.bat
+++ qt_designer/make_uis.bat
@@ -1,5 +1,8 @@
 pyside2-uic.exe .\qt_bcars2_raster.ui -o ..\bcars_microscope\ui\ui_bcars2_raster.py
 pyside2-uic.exe .\qt_bcars2_spectroscopy.ui -o ..\bcars_microscope\ui\ui_bcars2_spectroscopy.py
 pyside2-uic.exe .\qt_demo_2_threads.ui -o ..\bcars_microscope\ui\ui_demo_2_threads.py
+pyside2-uic.exe .\qt_andor_setup.ui -o ..\bcars_microscope\ui\ui_andor_setup.py
 pyside2-uic.exe .\qt_bcars2_main.ui -o ..\bcars_microscope\ui\ui_bcars2_main.py
+
+
 pyside2-uic.exe .\qt_learn_multithread.ui -o ..\bcars_microscope\dev_edu\ui_learn_multithread.py
