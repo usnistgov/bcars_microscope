4a45aff6f5d52206c3e7e3a5e4aae601f76d3720
Charles H. Camp Jr <charles.camp@nist.gov>
2022-08-01 16:27:19 -0400

    Spectrometer UI

--- qt_designer/make_uis.bat
+++ qt_designer/make_uis.bat
@@ -1,21 +1,21 @@
-call pyuic5.bat .\qt_bcars2_raster.ui -o ..\bcars_microscope\ui\ui_bcars2_raster.py
+call pyuic5.bat .\qt_designer\qt_bcars2_raster.ui -o .\bcars_microscope\ui\ui_bcars2_raster.py
 
-call pyuic5.bat .\qt_bcars2_macro.ui -o ..\bcars_microscope\ui\ui_bcars2_macro.py
-call pyuic5.bat .\qt_bcars2_timescan.ui -o ..\bcars_microscope\ui\ui_bcars2_timescan.py
-call pyuic5.bat .\qt_bcars2_spectroscopy.ui -o ..\bcars_microscope\ui\ui_bcars2_spectroscopy.py
-call pyuic5.bat .\qt_demo_2_threads.ui -o ..\bcars_microscope\ui\ui_demo_2_threads.py
+call pyuic5.bat .\qt_designer\qt_bcars2_macro.ui -o .\bcars_microscope\ui\ui_bcars2_macro.py
+call pyuic5.bat .\qt_designer\qt_bcars2_timescan.ui -o .\bcars_microscope\ui\ui_bcars2_timescan.py
+call pyuic5.bat .\qt_designer\qt_bcars2_spectroscopy.ui -o .\bcars_microscope\ui\ui_bcars2_spectroscopy.py
+call pyuic5.bat .\qt_designer\qt_demo_2_threads.ui -o .\bcars_microscope\ui\ui_demo_2_threads.py
 
-call pyuic5.bat .\qt_andor_setup.ui -o ..\bcars_microscope\ui\ui_andor_setup.py
-call pyuic5.bat .\qt_bcars2_esp.ui -o ..\bcars_microscope\ui\ui_bcars2_esp.py
-call pyuic5.bat .\qt_bcars2_pi_macro.ui -o ..\bcars_microscope\ui\ui_bcars2_pi_macro.py
-call pyuic5.bat .\qt_bcars2_pi_nano.ui -o ..\bcars_microscope\ui\ui_bcars2_pi_nano.py
+call pyuic5.bat .\qt_designer\qt_andor_setup.ui -o .\bcars_microscope\ui\ui_andor_setup.py
+call pyuic5.bat .\qt_designer\qt_bcars2_esp.ui -o .\bcars_microscope\ui\ui_bcars2_esp.py
+call pyuic5.bat .\qt_designer\qt_bcars2_pi_macro.ui -o .\bcars_microscope\ui\ui_bcars2_pi_macro.py
+call pyuic5.bat .\qt_designer\qt_bcars2_pi_nano.ui -o .\bcars_microscope\ui\ui_bcars2_pi_nano.py
 
 
-call pyuic5.bat .\qt_bcars2_main.ui -o ..\bcars_microscope\ui\ui_bcars2_main.py
+call pyuic5.bat .\qt_designer\qt_bcars2_main.ui -o .\bcars_microscope\ui\ui_bcars2_main.py
 
-call pyuic5.bat .\qt_bcars2_spectrometer.ui -o ..\bcars_microscope\ui\ui_bcars2_spectrometer.py
-call pyuic5.bat .\qt_bcars2_laser_setup.ui -o ..\bcars_microscope\ui\ui_bcars2_laser_setup.py
+call pyuic5.bat .\qt_designer\qt_bcars2_spectrometer.ui -o .\bcars_microscope\ui\ui_bcars2_spectrometer.py
+call pyuic5.bat .\qt_designer\qt_bcars2_laser_setup.ui -o .\bcars_microscope\ui\ui_bcars2_laser_setup.py
 
-call pyuic5.bat .\qt_learn_multithread.ui -o ..\bcars_microscope\dev_edu\ui_learn_multithread.py
+call pyuic5.bat .\qt_designer\qt_learn_multithread.ui -o .\bcars_microscope\dev_edu\ui_learn_multithread.py
 
 
